6bc1caa2becff076ddd524fe4f7cfdf08f728185
Sebastian Pietras <spietras1998@gmail.com>
2022-02-16 20:49:55 +0100

    Applied formatting

--- emipass/src/main.js
+++ emipass/src/main.js
@@ -1,43 +1,55 @@
-import child_process from 'child_process';
-import geckos from '@geckos.io/server';
+import child_process from "child_process";
+import geckos from "@geckos.io/server";
 
-const sourcePort = process.env.EMIPASS_SOURCE_PORT || 10000;
-const minSourceDataPort = process.env.EMIPASS_MIN_SOURCE_DATA_PORT || 10001;
-const maxSourceDataPort = process.env.EMIPASS_MAX_SOURCE_DATA_PORT || 10099;
-const targetHost = process.env.EMIPASS_TARGET_HOST || 'localhost';
+const sourcePort = process.env.PORT || 10000;
+const sourceDataPort = process.env.DATA_PORT || 10001;
+const targetHost = process.env.EMIPASS_TARGET_HOST || "localhost";
 const targetPort = process.env.EMIPASS_TARGET_PORT || 9000;
 
-const io = geckos({ portRange: { min: minSourceDataPort, max: maxSourceDataPort } });
+const io = geckos({
+  portRange: { min: sourceDataPort, max: sourceDataPort },
+});
 
 io.listen(sourcePort);
 
-io.onConnection(channel => {
+io.onConnection((channel) => {
+  const ffmpeg = child_process.spawn("ffmpeg", [
+    "-i",
+    "-",
+    "-acodec",
+    "libopus",
+    "-f",
+    "ogg",
+    "-loglevel",
+    "debug",
+    `srt://${targetHost}:${targetPort}`,
+  ]);
 
-    const ffmpeg = child_process.spawn('ffmpeg', [
-        '-i', '-',
-        '-acodec', 'libopus',
-        '-f', 'ogg',
-        `srt://${targetHost}:${targetPort}`
-    ]);
+  ffmpeg.on("exit", (code, signal) => {
+    console.log(
+      "FFmpeg child process closed, code " + code + ", signal " + signal
+    );
+    channel.close().then();
+  });
 
-    ffmpeg.on('close', (code, signal) => {
-        console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
-        channel.close();
-    });
+  ffmpeg.stdin.on("error", (e) => {
+    console.log("FFmpeg STDIN Error", e);
+  });
 
-    ffmpeg.stdin.on('error', (e) => {
-        console.log('FFmpeg STDIN Error', e);
-    });
+  ffmpeg.stderr.on("data", (data) => {
+    console.log("FFmpeg STDERR:", data.toString());
+  });
 
-    ffmpeg.stderr.on('data', (data) => {
-        console.log('FFmpeg STDERR:', data.toString());
-    });
+  channel.onDrop(() => {
+    console.log("Message dropped");
+  });
 
-    channel.onRaw(rawMessage => {
-        ffmpeg.stdin.write(rawMessage);
-    });
+  channel.onRaw((rawMessage) => {
+    ffmpeg.stdin.write(rawMessage);
+  });
 
-    channel.onDisconnect(() => {
-        ffmpeg.kill('SIGINT');
-    });
+  channel.onDisconnect(() => {
+    console.log("Channel disconnected");
+    ffmpeg.kill("SIGKILL");
+  });
 });
