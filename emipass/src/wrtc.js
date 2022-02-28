a65948d6cf40c53e01dee0d08949e7d8728f9004
Sebastian Pietras <spietras1998@gmail.com>
2022-02-28 13:16:50 +0100

    Added option to use STUN servers

--- emipass/src/wrtc.js
+++ emipass/src/wrtc.js
@@ -1,10 +1,11 @@
-import geckos from "@geckos.io/server";
+import geckos, { iceServers } from "@geckos.io/server";
 
 export default class WRTCServer {
-  constructor(port) {
+  constructor(port, useStun = false) {
     this.port = port;
     this.server = geckos({
       portRange: { min: port, max: port },
+      iceServers: useStun ? iceServers : [],
     });
     this.server.onConnection((channel) => this.connectionCallback(channel));
   }
